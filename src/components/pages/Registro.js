be2d80aae9ebf204cb5a9a17407f693bef8d95b4
UrbinaBarrios <urbinabarios@gmail.com>
2021-11-10 12:44:25 -0400

    Arreglado Registro implementado User Context

--- src/components/pages/Registro.js
+++ src/components/pages/Registro.js
@@ -1,32 +1,51 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import './Registro.css';
-import  firebaseApp  from '../../firebaseApp';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { Icon } from './Icon';
-import {
+import React, { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./Registro.css";
+import { UserContext } from "../../context/UserContext";
+import { auth } from "../../firebaseApp";
+import { Icon } from "./Icon";
 
-  Publish,
-} from "@material-ui/icons";
-const Registro = ({ handleClose }) => {
-  // Crear variables para cada input
-  const [firstName, setFirstName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmed_password, setConfirmed_Password] = useState('');
-  const [gender, setGender] = useState('');
-  const auth = new getAuth();
-  const router = useHistory();
-  
+const Registro = () => {
+  const history = useHistory();
+  const { createUser } = useContext(UserContext);
+
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    confirmed_password: "",
+    gender: "",
+  });
+
+  const handleOnChange = (event) => {
+    const { value, name: inputName } = event.target;
+    console.log({ inputName, value });
+    setValues({ ...values, [inputName]: value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    //console.log(firstName, phoneNumber, lastName, email, password, confirmed_password, gender);
-    const res = await createUserWithEmailAndPassword(auth, email, password).then(()=>{
-      router.push('/perfilusuario')
-    });
-    console.log(res)
+    const res = await auth.createUserWithEmailAndPassword(
+      values.email,
+      values.password
+    );
+
+    await createUser(
+      {
+        name: values.firstName,
+        lastname: values.lastName,
+        email: values.email,
+        gender: values.gender,
+        phoneNumber: values.phoneNumber,
+      },
+      res.user.uid
+    );
+
+    history.push("/");
+
+    console.log(res.user.uid);
   };
 
   return (
@@ -34,99 +53,131 @@ const Registro = ({ handleClose }) => {
       <h1 className="newUserTitle">Registrarse</h1>
       <p className="p1">Completa el formulario para completar tu cuenta</p>
       <form className="newUserForm" onSubmit={handleSubmit}>
-
         <div className="newUserItem">
           <label>Nombre</label>
-          <input type="text" placeholder="Jhon"
-          variant="filled"
-          required
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}/>
+          <input
+            name="firstName"
+            type="text"
+            placeholder="Introduzca su nombre"
+            variant="filled"
+            value={values.firstName}
+            onChange={handleOnChange}
+            required
+          />
         </div>
+
         <div className="newUserItem">
           <label>Apellido</label>
-          <input type="text" placeholder="Doe"
-          variant="filled"
-          required
-          value={lastName}
-          onChange={e => {
-          e.preventDefault()
-          setLastName(e.target.value)}}/>
+          <input
+            name="lastName"
+            type="text"
+            placeholder="Introduzca su Apellido"
+            variant="filled"
+            required
+            value={values.lastName}
+            onChange={handleOnChange}
+          />
         </div>
+
         <div className="newUserItem">
           <label>Numero De Teléfono</label>
-          <input type="number" placeholder="+58 (xxx) xxx xxxx"
-          variant="filled"
-          required
-          value={phoneNumber}
-          onChange={e => {
-          e.preventDefault()
-          setPhoneNumber(e.target.value)}}/>
+          <input
+            name="phoneNumber"
+            type="number"
+            placeholder="+58 (xxx) xxx xxxx"
+            variant="filled"
+            required
+            value={values.phoneNumber}
+            onChange={handleOnChange}
+          />
         </div>
+
         <div className="newUserItem">
           <label>Email</label>
-          <input type="email" placeholder="jhon@email.com"
-          variant="filled"
-          required
-          value={email}
-          onChange={e => setEmail(e.target.value)}/>
+          <input
+            name="email"
+            type="email"
+            placeholder="Introduzca su Email"
+            variant="filled"
+            required
+            value={values.email}
+            onChange={handleOnChange}
+          />
         </div>
+
         <div className="newUserItem">
           <label>Contraseña</label>
-          <input type="password" placeholder="xxxxxxx"
-          variant="filled"
-          required
-          value={password}
-          onChange={e => setPassword(e.target.value)}/>
+          <input
+            name="password"
+            type="password"
+            placeholder="xxxxxxx"
+            variant="filled"
+            required
+            value={values.password}
+            onChange={handleOnChange}
+          />
         </div>
+
         <div className="newUserItem">
-          <label>Contraseña</label>
-          <input type="password" placeholder="xxxxxxx"
-          variant="filled"
-          required
-          value={confirmed_password}
-          onChange={e => setConfirmed_Password(e.target.value)}/>
+          <label>Confirmar Contraseña</label>
+          <input
+            name="confirmed_password"
+            type="password"
+            placeholder="xxxxxxx"
+            variant="filled"
+            required
+            value={values.confirmed_password}
+            onChange={handleOnChange}
+          />
         </div>
+
         <div className="newUserItem">
           <label>Genero</label>
           <div className="newUserGender">
-            <input type="radio"  id="gender" value="male" onChange={e=>
-            {e.preventDefault()
-            setGender(e.target.value)
-            }
-            }/>
+            <input
+              name="gender"
+              type="radio"
+              id="gender"
+              value="male"
+              onChange={handleOnChange}
+            />
             <label for="male">Hombre</label>
-            <input type="radio" name="gender" id="female" value="female" onChange={e=>
-            {e.preventDefault()
-            setGender(e.target.value)
-            }
-            }/>
+            <input
+              type="radio"
+              name="gender"
+              id="female"
+              value="female"
+              onChange={handleOnChange}
+            />
             <label for="female">Mujer</label>
-            <input type="radio" name="gender" id="other" value="other" onChange={e=>
-            {e.preventDefault()
-            setGender(e.target.value)
-            }
-            }/>
+            <input
+              type="radio"
+              name="gender"
+              id="other"
+              value="other"
+              onChange={handleOnChange}
+            />
             <label for="other">Otro</label>
           </div>
         </div>
-      <div >
-        <br/>
-        <br/>
-        <div className='boton-registro'>
-        <button className='link' type='submit'>Registrarse</button>
-        <Link className='link' to='/'>
-          Cancelar
-        </Link>
-        <Link className='link' to='/registropsico'>
-          Eres Psicologo?
-        </Link>
-        
+
+        <div>
+          <br />
+          <br />
+          <div className="boton-registro">
+            <button className="link" type="submit">
+              Registrarse
+            </button>
+            <Link className="link" to="/">
+              Cancelar
+            </Link>
+            <Link className="link" to="/registropsico">
+              Eres Psicologo?
+            </Link>
           </div>
         </div>
-    
-    </form>
-    <Icon />
+      </form>
+      <Icon />
     </div>
   );
 };
