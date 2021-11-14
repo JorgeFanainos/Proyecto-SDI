1e61afb16f2c02591aca29a425de39dc4aae6102
UrbinaBarrios <urbinabarios@gmail.com>
2021-11-14 15:58:37 -0400

    changed img path

--- src/components/pages/BuscarPsicologos/Carta.js
+++ src/components/pages/BuscarPsicologos/Carta.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './Carta.css'
+import React from "react";
+import "./Carta.css";
 
-
-function Carta({person}) {
-  return(
+function Carta({ person }) {
+  return (
     <div className="contenedorCarta">
       <img className="perfilPsicologo" alt="" src={person.imgPath} />
       <div>
-        <h2 className='InfoPsicologo'>{person.name}</h2>
-        <p className='InfoPsicologo'>{person.especialidad}</p>
+        <h2 className="InfoPsicologo">{person.name}</h2>
+        <p className="InfoPsicologo">{person.especialidad}</p>
       </div>
     </div>
   );
--- src/components/pages/BuscarPsicologos/InitailData.js
+++ src/components/pages/BuscarPsicologos/InitailData.js
@@ -1,60 +1,60 @@
 const initialDetails = [
-    {
-      id: 1,
-      imgPath: "/public/images/img-1.jpg",
-      name: "Jane Doe",
-      especialidad: "Psiquiatra",
-      address: "Direccion",
-    },
-    {
-      id: 2,
-      imgPath: "/public/images/img-2.jpg",
-      name: "Mary Rosamund",
-      especialidad: "Neuropsicólogo",
-      address: "Direccion",
-    },
-    {
-      id: 3,
-      imgPath: "/public/images/img-3.jpg",
-      name: "Sherlock Watson",
-      especialidad: "Especialista en ansiedad",
-      address: "Direccion",
-    },
-    {
-      id: 4,
-      imgPath: "/public/images/img-4.jpg",
-      name: "John Holmes",
-      especialidad: "Psicoterapeuta",
-      address: "Direccion",
-    },
-    {
-      id: 5,
-      imgPath: "/public/images/img-5.jpg",
-      name: "Mycroft Lestrade",
-      especialidad: "Psicólogo de desarrollo",
-      address: "Direccion",
-    },
-    {
-      id: 6,
-      imgPath: "/public/images/img-6.jpg",
-      name: "Andres Gonzales",
-      especialidad: "Psicoterapeuta",
-      address: "Direccion",
-    },
-    {
-      id: 7,
-      imgPath: "/public/images/img-6.jpg",
-      name: "Alejandro Ramirez",
-      especialidad: "Psicólogo educativo",
-      address: "Direccion",
-    },
-    {
-      id: 8,
-      imgPath: "/public/images/img-6.jpg",
-      name: "Jorge Perez",
-      especialidad: "Neuropsicólogo",
-      address: "Direccion",
-    },
-  ];
-  
-  export default initialDetails;
+  {
+    id: 1,
+    imgPath: "../../../../public/images/img-1.jpg",
+    name: "Jane Doe",
+    especialidad: "Psiquiatra",
+    address: "Direccion",
+  },
+  {
+    id: 2,
+    imgPath: "../../../../public/images/img-2.jpg",
+    name: "Mary Rosamund",
+    especialidad: "Neuropsicólogo",
+    address: "Direccion",
+  },
+  {
+    id: 3,
+    imgPath: "../../../../public/images/img-3.jpg",
+    name: "Sherlock Watson",
+    especialidad: "Especialista en ansiedad",
+    address: "Direccion",
+  },
+  {
+    id: 4,
+    imgPath: "../../../../public/images/img-4.jpg",
+    name: "John Holmes",
+    especialidad: "Psicoterapeuta",
+    address: "Direccion",
+  },
+  {
+    id: 5,
+    imgPath: "../../../../public/images/img-5.jpg",
+    name: "Mycroft Lestrade",
+    especialidad: "Psicólogo de desarrollo",
+    address: "Direccion",
+  },
+  {
+    id: 6,
+    imgPath: "../../../../public/images/img-6.jpg",
+    name: "Andres Gonzales",
+    especialidad: "Psicoterapeuta",
+    address: "Direccion",
+  },
+  {
+    id: 7,
+    imgPath: "../../../../public/images/img-6.jpg",
+    name: "Alejandro Ramirez",
+    especialidad: "Psicólogo educativo",
+    address: "Direccion",
+  },
+  {
+    id: 8,
+    imgPath: "../../../../public/images/img-6.jpg",
+    name: "Jorge Perez",
+    especialidad: "Neuropsicólogo",
+    address: "Direccion",
+  },
+];
+
+export default initialDetails;
