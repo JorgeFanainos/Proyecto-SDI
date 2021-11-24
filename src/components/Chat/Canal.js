3b25bd002db585bee7219bcf41c76495abeddabb
Jorge Fanianos <jorgefanianos@Jorges-Mac-mini.local>
2021-11-24 14:00:32 -0400

    arregleado color de psicologos en Hero section

--- src/components/Chat/Canal.js
+++ src/components/Chat/Canal.js
@@ -7,13 +7,67 @@ import { useFirestoreQuery } from './Hooks';
 import { db } from "../../utils/firebaseApp";
 
 const Channel = ({ user = null }) => {
+    const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState([]);
-    useEffect(()=>{
+    const { uid, displayName, photoURL } = user;
+    useEffect(() => {
         if (db) {
-            
+         const unsuscribe =  db
+            .collection('messages')
+            .orderBy('createdAT')
+            .limit(150);
+            .onSnapshot(querySnapshot => {
+                // Get all documents from collection - with IDs
+                const data = querySnapshot.docs.map(doc => ({
+                  ...doc.data(),
+                  id: doc.id,
+                }));
+              });
+           return unsuscribe;
         }
-    })
+    }, [db]);
+    const handleOnChange = e => {
+        setNewMessage(e.target.value)
+    };
+    const handleOnSumit = e => {
+        e.preventDefault();
+        if (db){
+            db.collection('messages').add({
+                text:newMessage,
+                createdAT: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL
+            })
+        }
+    };
+    return (
+    <>
+      <ul>
+        {messages.map(message => (
+          <li key={message.id}>{message.text}</li>
+        ))}
+      </ul>
+      <form
+        onSubmit={handleOnSubmit}>
+        <input
+          ref={inputRef}
+          type="text"
+          value={newMessage}
+          onChange={handleOnChange}
+          placeholder="Type your message here..."
+        />
+        <button
+          type="submit"
+          disabled={!newMessage}
+        >
+          Send
+        </button>
+      </form>
+    </>
+        
     );
+};
   
     
   
